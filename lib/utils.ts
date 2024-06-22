6ba4328396e76ebb9e2bb896511164caf78165db
Fredrik Johansson <42123219+fjohanssondev@users.noreply.github.com>
2024-06-22 23:50:49 +0200

    feat: added utils to fetch teams by team-id or user-id

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { User, UserTeam } from "@prisma/client"
+import { db } from "~/lib/db"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -6,12 +6,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function constructLeaderboard(users: UserWithTeams[]) {
+/* export function constructLeaderboard(users: Player[]) {
   return users.map(player => {
     return {
       name: player.name,
-      captain: player.userTeam.map(({ captain }) => captain),
-      points: player.userTeam.reduce((acc: number, curr: UserTeam) => acc + curr.points, 0)
+      captain: player.tea.map(({ captain }) => captain),
+      points: player.userTeam.reduce((acc: number, curr: Team) => acc + curr.points, 0)
     }
   }).sort((a, b) => b.points - a.points)
 }
@@ -24,4 +24,28 @@ export function formatTimeLeft(milliseconds: number) {
   const seconds = totalSeconds % 60
 
   return `${days}d ${hours}h ${minutes}m ${seconds}s`
+}
+
+export function checkIfUserHasTeamForCurrentSeason(user: any) {
+  return user.teams.some((team: Team) => team. === process.env.CURRENT_SEASON)
+} */
+
+export async function waitUntil(interval = 100) {
+  return new Promise(resolve => setTimeout(resolve, interval))
+}
+
+export async function getTeamByUser(userId: string) {
+  return await db.team.findFirst({
+    where: {
+      userId
+    }
+  })
+}
+
+export async function getTeamById(id: string) {
+  return await db.team.findFirst({
+    where: {
+      id
+    }
+  })
 }
