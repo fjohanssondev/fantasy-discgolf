f3def815e45ae410e60c7e6d52a9340547fd6e30
Fredrik Johansson <42123219+fjohanssondev@users.noreply.github.com>
2024-06-22 23:52:10 +0200

    wip: render avatar and username

--- components/signin.tsx
+++ components/signin.tsx
@@ -1,11 +1,19 @@
 import { auth, signIn } from "~/auth"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
  
 export async function SignIn() {
-
   const session = await auth()
 
-  if (session?.user) return <p>Logged in as: <span className="font-semibold">{session?.user.name}</span></p>
-
+  if (session?.user) return (
+    <div className="flex items-center space-x-2">
+      <Avatar>
+        <AvatarImage src={session?.user.image ?? ""} />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <span className="font-medium">{session?.user.name}</span>
+    </div>
+  )
+    
   return (
     <form
       action={async () => {
