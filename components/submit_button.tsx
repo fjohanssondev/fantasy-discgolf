92c052f0eda23a890b9b9d70088370217c25a9c1
Fredrik Johansson <42123219+fjohanssondev@users.noreply.github.com>
2024-06-23 23:46:56 +0200

    fix: added button props

--- components/submit_button.tsx
+++ components/submit_button.tsx
@@ -4,9 +4,14 @@ import { useFormStatus } from "react-dom"
 import { Button } from "~/components/ui/button"
 import { LoaderCircle } from "lucide-react"
 
-export default function Submit({ children }: { children?: React.ReactNode }) {
+interface SubmitProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+  children: React.ReactNode
+}
+
+
+export default function Submit({ children, ...props }: SubmitProps) {
 
   const { pending } = useFormStatus()
 
-  return <Button type="submit" disabled={pending}>{pending ? <LoaderCircle className="animate-spin" /> : children}</Button>
+  return <Button {...props} type="submit" disabled={pending}>{pending ? <LoaderCircle className="animate-spin" /> : children}</Button>
 }
